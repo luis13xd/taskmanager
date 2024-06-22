f61a34e479744884e7e3f17aaeb8bea2f9affdc0
luis13xd <105569523+luis13xd@users.noreply.github.com>
2024-06-22 00:34:01 -0500

    Update db.php

--- backend/config/db.php
+++ backend/config/db.php
@@ -13,7 +13,6 @@ class Database {
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
-            echo "Conexión exitosa"; 
         } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
