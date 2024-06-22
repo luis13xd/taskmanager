b0e410423a45275ced766accb34290275838e5ce
luis13xd <105569523+luis13xd@users.noreply.github.com>
2024-06-22 00:35:31 -0500

    Update scripts.js

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,4 +1,4 @@
-// frontend/js/scripts.js
+
 function filterTasks(filter) {
     const tasks = document.querySelectorAll('.task');
 
@@ -14,20 +14,3 @@ function filterTasks(filter) {
         }
     });
 }
-
-
-// function filterTasks(filter) {
-//     const tasks = document.querySelectorAll('.task');
-
-//     tasks.forEach(task => {
-//         if (filter === 'all') {
-//             task.style.display = 'block';
-//         } else if (filter === 'completed' && task.getAttribute('data-completed') === '1') {
-//             task.style.display = 'block';
-//         } else if (filter === 'pending' && task.getAttribute('data-completed') === '0') {
-//             task.style.display = 'block';
-//         } else {
-//             task.style.display = 'none';
-//         }
-//     });
-// }
